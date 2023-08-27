1e200b9157c2d8319e28e385da25f081f206fb05
Corvus278 <makar.korolenko2005@mail.ru>
2023-08-27 14:40:05 +0300

    add webpack building

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+ // webpack.config.js
+ const path = require('path');
+
+ module.exports = {
+   target: 'node', 
+   entry: './src/index.ts', 
+   output: {
+     filename: 'bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+   },
+   module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+ };
